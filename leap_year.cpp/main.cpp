b4e9ee8ecebc7a605087f1ee8876b59fc37cb476
briangrffn <132273377+briangrffn@users.noreply.github.com>
2023-05-21 12:11:49 +0500

    Add files via upload

---
+++ leap_year.cpp/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter a year: ";
+    //takes an input in 4-digit number
+    cin >> n;
+
+    if(n % 4 == 0 && n % 100 != 0 || n % 400 == 0){
+        cout << n << " falls on a year.\n";
+    }
+    else if (n < 1000 || n > 9999){
+        cout << "Invalid entry.\n";
+    }
+    else {
+        cout << n << " is not a leap year.\n";
+    }
+
+
+    return 0;
+}
