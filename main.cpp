9d83d0d5b8d918d128faba0738da9ff38c62aeca
briangrffn <132273377+briangrffn@users.noreply.github.com>
2023-05-23 10:55:27 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//#7 - Project | Monday May 22, 2023
+#include <iostream>
+
+using namespace std;
+int main() {
+
+    //Quadratic Formula
+
+    int n, quadrat;
+    cin >> n;
+    for (int i = 1; i <= 1; i++) {
+
+        quadrat = n * n;
+        cout << quadrat << " ";
+    }
+
+
+
+
+    return 0;
+}
